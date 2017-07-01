f6478eb7ffd004e200a914922470329f82a12b4b
ShawnXxy <shawnxxy@hotmail.com>
2017-07-01 18:27:24 +0800

    shape factory

--- src/factory/ShapeFactory.java
+++ src/factory/ShapeFactory.java
@@ -3,7 +3,21 @@ package factory;
 public class ShapeFactory {
 	
 	// use getShape method to get object to type shape
-
+	public Shape getShape (String shapeType) {
+		if (shapeType == null) {
+			return null;
+		}
+		if (shapeType.equalsIgnoreCase("CIRCLE")) {
+			return new Circle();
+		} else if (shapeType.equalsIgnoreCase("RECTANGLE")) {
+			return new Rectangle();
+		} else if (shapeType.equalsIgnoreCase("SQUARE")) {
+			return new Square();
+		}
+		return null;
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
