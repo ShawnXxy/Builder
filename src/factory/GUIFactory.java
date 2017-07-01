6d96d5069aa9a70022f4fe9d6b901deaecab9d86
ShawnXxy <shawnxxy@hotmail.com>
2017-07-01 18:36:55 +0800

    GUI factory

---
+++ src/factory/GUIFactory.java
@@ -0,0 +1,14 @@
+package factory;
+
+public interface GUIFactory {
+	public Button createButton();
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
